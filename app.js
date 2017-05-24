821589987358382a921a01dc890db2f639bf5556
jcaffaro <jcaffaro@muxi.com.br>
2017-05-24 16:44:03 -0300

    commit for client and checking if it is got characters

--- app.js
+++ app.js
@@ -18,5 +18,24 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    var char = session.message.text;
+    if(hasGotChar(char))
+    {
+      var client = restify.createJsonClient({
+        url:  'https://got-quotes.herokuapp.com'
+      });
+      console.log("Char = " + char);
+      client.get('/quotes?char=' + char, function(err, req, res, obj){
+        console.log("Quote " + obj.quote);
+        session.send(obj.quote);
+      });
+    }else{
+      session.send("Nenhum personagem de Game Of Thrones", session.message.text);
+    }
 });
+
+function hasGotChar(char){
+  char = char.toUpperCase();
+  var chars = ['tyrion', 'cersei', 'bronn', 'brynden', 'hound', 'jaime', 'littlefinger', 'olenna', 'renly', 'varys'];
+  return char.indexOf(char) != -1;
+};
